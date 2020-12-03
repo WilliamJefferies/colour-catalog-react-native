439b0d89d674f57e04f250e2a0f877f7dc4533cb
William Jefferies <williamjefferies@Williams-MacBook-Pro.local>
2020-12-03 16:50:30 +1100

    implemented add colour

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { View, StyleSheet, FlatList } from "react-native";
+import { generate } from "shortid";
 
 import ColourButton from "./Components/ColourButton";
 import ColourForm from "./Components/ColourForm";
@@ -7,13 +8,13 @@ import ColourForm from "./Components/ColourForm";
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState("blue");
   const [colours, setColours] = useState([]);
+  const addColour = (colour) => {
+    const newColour = { id: generate(), colour };
+    setColours([newColour, ...colours]);
+  };
   return (
     <>
-      <ColourForm
-        onNewColour={(newColour) =>
-          Alert.alert(`TODO: add colour ${newColour}`)
-        }
-      />
+      <ColourForm onNewColour={addColour} />
       <FlatList
         style={[styles.container, { backgroundColor }]}
         data={colours}
